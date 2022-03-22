1588ab5b8b9104945e407994dcff3096435baeb6
Hans-Christian Otto <c.otto@suora.com>
2022-03-22 12:28:58 +0100

    Added TimeEntryView component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import TimeEntryView from "./components/TimeEntryView";
 
 function App() {
-  return <h1>Florian</h1>;
+  return (
+    <div>
+      <h1>Florian</h1>
+      <TimeEntryView comment="React ist knorke." />
+      <TimeEntryView comment="React ist töfte." />
+    </div>
+  );
 }
 
 export default App;
