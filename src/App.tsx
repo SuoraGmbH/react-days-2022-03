ac00476f21a2efcbcb42a8cd3e32ec516f8cf506
Hans-Christian Otto <c.otto@suora.com>
2022-03-22 14:15:07 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import TimeEntryView from "./components/TimeEntryView";
 import Welcome from "./components/Welcome";
+import { TimeEntry } from "./domain/TimeEntry";
 
 function App() {
+  const timeEntry: TimeEntry = {
+    id: "dhosufnoudfsohdsajioadsio",
+    comment: "München ist eine schöne Stadt!",
+    start: new Date(),
+    end: new Date(),
+  };
+
   return (
     <div>
       <Welcome city="Munich" />
       <Welcome />
       <h1>Florian</h1>
-      <TimeEntryView
-        timeEntry={{
-          id: "dhosufnoudfsohdsajioadsio",
-          comment: "München ist eine schöne Stadt!",
-          start: new Date(),
-          end: new Date(),
-        }}
-      />
+      <TimeEntryView timeEntry={timeEntry} />
     </div>
   );
 }
