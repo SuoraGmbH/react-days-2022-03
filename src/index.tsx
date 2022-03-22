b6a29159cf40ab9a4c734a845869848f22b2f34d
Hans-Christian Otto <c.otto@suora.com>
2022-03-22 11:46:18 +0100

    Added App Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 
-const element = <h1>Hallo React-Days "München"!</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
