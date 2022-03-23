9a8d010092712e2d2c3d5ebd445ae0e5ea4e193f
Hans-Christian Otto <c.otto@suora.com>
2022-03-23 14:57:21 +0100

    WIP: convert time entries

--- src/hooks/useTimeEntriesFromServer.ts
+++ src/hooks/useTimeEntriesFromServer.ts
@@ -4,14 +4,32 @@ import { useEffect, useState } from "react";
 interface TimeEntriesFromServerReturnValue {
   timeEntries: TimeEntry[];
 }
+
+interface TimeEntryBackend {
+  id: string;
+  start: string;
+  end: string;
+  comment: string;
+}
+
 const useTimeEntriesFromServer = (): TimeEntriesFromServerReturnValue => {
   const [timeEntries, setTimeEntries] = useState<TimeEntry[]>();
 
   useEffect(() => {
     fetch("http://localhost:3001/timeEntries")
       .then((response) => response.json())
-      .then((data: TimeEntry[]) => {
-        setTimeEntries(data);
+      .then((timeEntriesBackend: TimeEntryBackend[]) => {
+        const timeEntriesFrontend = timeEntriesBackend.map(
+          (timeEntry): TimeEntry => {
+            return {
+              id: timeEntry.id,
+              comment: timeEntry.comment,
+              start: new Date(timeEntry.start),
+              end: new Date(timeEntry.end),
+            };
+          }
+        );
+        setTimeEntries(timeEntriesFrontend);
       });
   }, []);
 
