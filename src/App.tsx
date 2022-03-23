96e35d23952aa4e49b980198cf3f36394a4081f8
Hans-Christian Otto <c.otto@suora.com>
2022-03-23 11:55:49 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
-import TimeEntryView from "./components/TimeEntryView";
-import Welcome from "./components/Welcome";
 import { TimeEntry } from "./domain/TimeEntry";
 import TimeEntryForm from "./components/TimeEntryForm";
-import Counter from "./components/Counter";
-import GithubStats from "./components/GithubStats";
-import DynamicGithubStats from "./components/DynamicGithubStats";
 import TimeEntryList from "./components/TimeEntryList";
 
 function App() {
