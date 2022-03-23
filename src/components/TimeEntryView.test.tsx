46586ae200198058cbfbed8dfc6b997637e2362e
Hans-Christian Otto <c.otto@suora.com>
2022-03-23 16:34:32 +0100

    Added test including snapshot

---
+++ src/components/TimeEntryView.test.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import TimeEntryView from "./TimeEntryView";
+import { TimeEntry } from "../domain/TimeEntry";
+
+const timeEntry: TimeEntry = {
+  id: "123213321",
+  comment: "Hallo Jennifer",
+  start: new Date("2022-03-23T15:30:30.526Z"),
+  end: new Date("2022-03-23T15:45:30.526Z"),
+};
+
+describe("<TimeEntryView />", function () {
+  test("renders without crashing", () => {
+    render(<TimeEntryView timeEntry={timeEntry} />);
+  });
+
+  test("renders according to snapshot", () => {
+    const { container } = render(<TimeEntryView timeEntry={timeEntry} />);
+    expect(container).toMatchSnapshot();
+  });
+});
